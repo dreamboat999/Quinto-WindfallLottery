28ddbd7bf861aa8c50a765eee08fbc8cc41fda54
dreamboat999 <kyleyutaka@gmail.com>
2023-05-05 19:03:15 -0400

    update

---
+++ src/pages/Home/Home copy.jsx
@@ -0,0 +1,201 @@
+import { useEffect, useState } from 'react';
+import { HashLoader } from 'react-spinners';
+import ResultsTable from 'components/ResultTable';
+import TableList from 'components/TableList';
+import ConnectionStatus from 'components/ConnectionStatus';
+import { validateJson, isJson } from 'utils/validateJson.js';
+import { MainLayout } from 'layouts/MainLayout';
+import styles from './home.module.scss';
+import { useSelector, useDispatch } from 'react-redux';
+import { setConnectedStatus } from 'slices/quintoSlices';
+const ws = new WebSocket('wss://qa.quinto.games');
+var primaryKeyList = [];
+
+const Home = ({ db }) => {
+  const dispatch = useDispatch();
+  const [error, setError] = useState(null);
+  const [results, setResults] = useState([]);
+  const [reqMsg, setReqMsg] = useState('');
+  const [tblList, setTblList] = useState([]);
+  const [curTable, setCurTable] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+  const [isDBLoaded, setIsDBLoaded] = useState(false);
+  const connected = useSelector((state) => state.quintoSlices.connected);
+  const exec = (sql) => {
+    let results;
+    try {
+      results = db.exec(sql);
+      setError(null);
+    } catch (err) {
+      setError(err);
+      results = [];
+    }
+    return results;
+  };
+
+  useEffect(() => {
+    getPrimaryKeys();
+  }, [tblList]);
+
+  const handleLoadDB = () => {
+    setIsLoading(true);
+    setTblList(
+      exec(
+        "SELECT name FROM sqlite_schema WHERE type ='table' AND name NOT LIKE 'sqlite_%';"
+      )
+    );
+    const initApiCall = { method: 'open_session' };
+    setTimeout(() => {
+      ws.send(JSON.stringify(initApiCall));
+    }, 500);
+    setIsDBLoaded(true);
+  };
+
+  const getPrimaryKeys = () => {
+    if (!tblList.length || !db) return;
+    for (let tbl of tblList[0].values) {
+      const res = db.exec(`SELECT DISTINCT ii.name AS 'indexed-columns'
+      FROM sqlite_schema AS m, pragma_index_list(m.name) AS il, pragma_index_info(il.name) AS ii
+      WHERE m.type='table' and m.name = '${tbl[0]}' order by 1;`);
+      const object = {};
+      Object.defineProperty(object, `${tbl[0]}`, { value: res[0].values });
+      primaryKeyList.push(object);
+    }
+  };
+
+  const handleChange = (e) => {
+    if (e.target.name === 'sql-query') setResults(exec(e.target.value));
+    if (e.target.name === 'request-message') setReqMsg(e.target.value);
+  };
+
+  const handleSendReqest = () => {
+    if (reqMsg) ws.send(reqMsg);
+  };
+
+  const handleDisconnect = () => {
+    ws.close();
+    dispatch(setConnectedStatus(ws.readyState));
+  };
+
+  ws.onopen = (event) => {
+    dispatch(setConnectedStatus(ws.readyState));
+  };
+  ws.onmessage = async function (event) {
+    dispatch(setConnectedStatus(ws.readyState));
+    if (!isJson(event.data)) {
+      setError(event.data);
+      setIsLoading(false);
+      return;
+    }
+    const json = JSON.parse(event.data);
+    if (!json.hasOwnProperty('result')) {
+      setIsLoading(false);
+      return;
+    }
+    console.log(json);
+    try {
+      const resultSQL = validateJson(json, primaryKeyList);
+      console.log(resultSQL);
+      if (db) {
+        for (let item of resultSQL) {
+          try {
+            await db.exec(item);
+          } catch (error) {
+            console.log(item);
+            console.log(error);
+          }
+        }
+        curTable && setResults(exec(`SELECT * from ${curTable}`));
+      }
+    } catch (err) {
+      console.log(json);
+      console.log(err);
+    }
+    setIsLoading(false);
+  };
+  ws.onerror = (event) => {
+    dispatch(setConnectedStatus(ws.readyState));
+  };
+  ws.onclose = (event) => {
+    dispatch(setConnectedStatus(ws.readyState));
+  };
+  return (
+    <MainLayout className={styles.home} title='Homepage'>
+      {isLoading && (
+        <div className='spinner'>
+          <HashLoader color='#03b3ff' />
+        </div>
+      )}
+
+      <main>
+        <div className='container'>
+          <div className='top-menu'>
+            <div className='input-control'>
+              <img src='assets/imgs/icon-search.svg' alt='search' />;
+              <input
+                onChange={handleChange}
+                name='sql-query'
+                placeholder='Enter some SQL query. Ex: “select sqlite_version()”'
+                spellCheck='false'
+              ></input>
+            </div>
+            <div className='input-control'>
+              <img src='assets/imgs/icon-send.svg' alt='send' />
+              <input
+                onChange={handleChange}
+                name='request-message'
+                placeholder='Enter messages to send request to the server'
+                spellCheck='false'
+                value={reqMsg}
+              />
+            </div>
+
+            <div className='button-group'>
+              <button
+                onClick={handleLoadDB}
+                className='filled'
+                disabled={isDBLoaded === true}
+              >
+                Load Schema
+              </button>
+              <button
+                onClick={handleSendReqest}
+                className='filled'
+                disabled={connected !== 1}
+              >
+                Send Request
+              </button>
+              <button
+                onClick={handleDisconnect}
+                className='filled'
+                disabled={connected !== 1}
+              >
+                Disconnect
+              </button>
+            </div>
+          </div>
+          <pre className='error'>{(error || '').toString()}</pre>
+          <section>
+            {tblList.length ? (
+              <TableList
+                tblList={tblList}
+                setCurTable={setCurTable}
+                exec={exec}
+                setResults={setResults}
+              />
+            ) : (
+              <></>
+            )}
+            {results.map(({ columns, values }, i) => (
+              <ResultsTable key={i} columns={columns} values={values} />
+            ))}
+          </section>
+        </div>
+      </main>
+      {/* <GoldCup /> */}
+      {/* <Rocket /> */}
+    </MainLayout>
+  );
+};
+
+export default Home;
---
+++ src/pages/Home/iterateNode.js
@@ -0,0 +1,375 @@
+/* IterateNode.js
+**
+** In JavaScript, objects are often represented using a notation called JSON (JavaScript Object Notation). JSON is a
+** lightweight data interchange format that is easy for humans to read and write and easy for machines to parse and generate.
+** 
+** In this case, the IterateNode class is designed to process results that are passed to it in the form of a JSON object.
+** This can be seen in the processResult method, which checks the type of the result parameter. If it is an array, the 
+** method assumes that each element of the array represents a separate JSON object, and processes each of these objects 
+** in turn. If the result parameter is not an array, the method assumes that it is a single JSON object and processes 
+** that instead.
+** 
+** Inside the handleOperation method, the mode parameter is used to determine what type of operation is being performed. 
+** This method expects the mode to be a string that represents the type of operation being performed, such as 'UPDATE',
+** 'INSERT', 'DELETE', or 'UPSERT'. The data parameter is then used to extract the relevant information for the specified
+** operation, such as the table name and the data to be inserted or updated.
+** 
+** Overall, the IterateNode class is designed to work with JSON objects that represent database operations, with each 
+** object containing a mode property that specifies the type of operation to be performed, and a data property that 
+** contains the relevant data for that operation.
+*/
+
+class IterateNode {
+  constructor(db) {
+    this.db = db;
+    this.ws = new WebSocket('wss://qa.quinto.games');
+    this.ws.addEventListener('open', this.onOpen.bind(this));
+    this.ws.addEventListener('message', this.onMessage.bind(this));
+    this.ws.addEventListener('close', this.onClose.bind(this));
+    this.ws.addEventListener('error', this.onError.bind(this));
+  }
+
+  onOpen(event) {
+    console.log('WebSocket connection established.');
+    const message = {"method":"open_session","params": {}};
+    this.ws.send(JSON.stringify(message));
+  }
+
+  onMessage(event) {
+    console.log(`Message received: ${event.data}`);
+    const json = JSON.parse(event.data);
+    this.processResult(json);
+  }
+
+  onClose(event) {
+    console.log('WebSocket connection closed.');
+    // Your code to handle WebSocket close event
+  }
+
+  onError(event) {
+    console.log(`WebSocket error: ${event.error}`);
+    // Your code to handle WebSocket error event
+  }
+  
+  async processResult(result) {
+    if (Array.isArray(result)) {
+      for (const obj of result) {
+        const [mode, data] = Object.entries(obj)[0];
+        await this.handleOperation(mode, data);
+      }
+    } else if (typeof result === 'object') {
+      const [mode, data] = Object.entries(result)[0];
+      await this.handleOperation(mode, data);
+    }
+  }
+
+  async handleOperation(mode, data) {
+    const tableName = Object.keys(data)[0];
+  
+    switch (mode.toUpperCase()) {
+      case 'UPDATE':
+        await this.handleUpdate(data);
+        break;
+      case 'INSERT':
+        await this.handleInsert(data);
+        break;
+      case 'DELETE':
+        await this.handleDelete(data);
+        break;
+      case 'UPSERT':
+        await this.handleUpsert(data);
+        break;
+      default:
+        throw new Error(`Invalid mode: ${mode}`);
+    }
+  }
+
+  async handleUpdate(data) {
+    const tableName = Object.keys(data)[0];
+    const rows = Array.isArray(data[tableName]) ? data[tableName] : [data[tableName]];
+    
+    for (const row of rows) {
+      await this.handleUpdateRow(tableName, row);
+    }
+  }
+  
+  async handleUpdateRow(tableName, row) {
+    const primaryKey = this.getPrimaryKey(tableName, row);
+    const existingRow = await this.getRow(tableName, primaryKey);
+  
+    if (!existingRow) {
+      // row doesn't exist, do nothing
+      return;
+    }
+  
+    const updatedRow = { ...existingRow, ...row };
+    await this.upsertRow(tableName, updatedRow);
+  }
+  
+  async handleInsert(data) {
+    const tableName = Object.keys(data)[0];
+    const rows = Array.isArray(data[tableName]) ? data[tableName] : [data[tableName]];
+  
+    for (const row of rows) {
+      await this.handleInsertRow(tableName, row);
+    }
+  }
+  
+  async handleInsertRow(tableName, row) {
+    const columns = Object.keys(row);
+    const values = Object.values(row);
+  
+    const sql = `INSERT INTO ${tableName} (${columns.join(',')}) VALUES (${columns.map(() => '?').join(',')})`;
+    await this.db.prepare(sql).run(values);
+  }
+  
+async handleDelete(data) {
+    const tableName = Object.keys(data)[0];
+    const whereClause = this.getWhereClause(tableName, data[tableName]);
+  
+    const sql = `DELETE FROM ${tableName} WHERE ${whereClause}`;
+    await this.db.prepare(sql).run();
+  }
+
+  async handleUpsert(data) {
+    const tableName = Object.keys(data)[0];class IterateNode {
+  constructor(db) {
+    this.db = db;
+  }
+
+  async processResult(result) {
+    if (Array.isArray(result)) {
+      for (const obj of result) {
+        const [mode, data] = Object.entries(obj)[0];
+        await this.handleOperation(mode, data);
+      }
+    } else if (typeof result === 'object') {
+      const [mode, data] = Object.entries(result)[0];
+      await this.handleOperation(mode, data);
+    }
+  }
+
+  async handleOperation(mode, data) {
+    const tableName = Object.keys(data)[0];
+  
+    switch (mode.toUpperCase()) {
+      case 'UPDATE':
+        await this.handleUpdate(data);
+        break;
+      case 'INSERT':
+        await this.handleInsert(data);
+        break;
+      case 'DELETE':
+        await this.handleDelete(data);
+        break;
+      case 'UPSERT':
+        await this.handleUpsert(data);
+        break;
+      default:
+        throw new Error(`Invalid mode: ${mode}`);
+    }
+  }
+
+  async handleUpdate(data) {
+    const tableName = Object.keys(data)[0];
+    const rows = Array.isArray(data[tableName]) ? data[tableName] : [data[tableName]];
+    
+    for (const row of rows) {
+      await this.handleUpdateRow(tableName, row);
+    }
+  }
+  
+  async handleUpdateRow(tableName, row) {
+    const primaryKey = this.getPrimaryKey(tableName, row);
+    const existingRow = await this.getRow(tableName, primaryKey);
+  
+    if (!existingRow) {
+      // row doesn't exist, do nothing
+      return;
+    }
+  
+    const updatedRow = { ...existingRow, ...row };
+    await this.upsertRow(tableName, updatedRow);
+  }
+  
+  async handleInsert(data) {
+    const tableName = Object.keys(data)[0];
+    const rows = Array.isArray(data[tableName]) ? data[tableName] : [data[tableName]];
+  
+    for (const row of rows) {
+      await this.handleInsertRow(tableName, row);
+    }
+  }
+  
+  async handleInsertRow(tableName, row) {
+    const columns = Object.keys(row);
+    const values = Object.values(row);
+  
+    const sql = `INSERT INTO ${tableName} (${columns.join(',')}) VALUES (${columns.map(() => '?').join(',')})`;
+    await this.db.prepare(sql).run(values);
+  }
+  
+async handleDelete(data) {
+    const tableName = Object.keys(data)[0];
+    const whereClause = this.getWhereClause(tableName, data[tableName]);
+  
+    const sql = `DELETE FROM ${tableName} WHERE ${whereClause}`;
+    await this.db.prepare(sql).run();
+  }
+
+  async handleUpsert(data) {
+    const tableName = Object.keys(data)[0];
+    const rows = Array.isArray(data[tableName]) ? data[tableName] : [data[tableName]];
+  
+    for (const row of rows) {
+      await this.handleUpsertRow(tableName, row);
+    }
+  }
+
+  async handleUpsertRow(tableName, row) {
+    const primaryKey = this.getPrimaryKey(tableName, row);
+    const existingRow = await this.getRow(tableName, primaryKey);
+  
+    if (!existingRow) {
+      await this.handleInsertRow(tableName, row);
+    } else {
+      const updatedRow = { ...existingRow, ...row };
+      await this.upsertRow(tableName, updatedRow);
+    }
+  }
+
+  async getRow(tableName, primaryKey) {
+    const sql = `SELECT * FROM ${tableName} WHERE ${this.getPrimaryKeyClause(tableName)} = ?`;
+    const result = await this.db.prepare(sql).get(primaryKey);
+    return result;
+  }
+
+  async upsertRow(tableName, row) {
+    const primaryKey = this.getPrimaryKey(tableName, row);
+    const columns = Object.keys(row);
+    const values = Object.values(row);
+  
+    const sql = `REPLACE INTO ${tableName} (${columns.join(',')}) VALUES (${columns.map(() => '?').join(',')})`;
+    await this.db.prepare(sql).run(values);
+  }
+
+  getPrimaryKey(tableName, row) {
+    const primaryKeyColumns = this.getPrimaryKeyColumns(tableName);
+    if (primaryKeyColumns.length === 1) {
+      return row[primaryKeyColumns[0]];
+    }
+  
+    return primaryKeyColumns.map(column => row[column]).join(',');
+  }
+
+  getPrimaryKeyColumns(tableName) {
+    const tableSchema = this.getTableSchema(tableName);
+    const primaryKey = tableSchema.find(column => column.pk);
+    return primaryKey ? primaryKey.name.split(',') : [];
+  }
+
+  getPrimaryKeyClause(tableName) {
+    const primaryKeyColumns = this.getPrimaryKeyColumns(tableName);
+    if (primaryKeyColumns.length === 1) {
+      return primaryKeyColumns[0];
+    }
+  
+    return primaryKeyColumns.map(column => `${column} = ?`).join(' AND ');
+  }
+
+  getTableSchema(tableName) {
+    const sql = `PRAGMA table_info(${tableName})`;
+    const result = this.db.prepare(sql).all();
+    return result;
+  }
+
+  getWhereClause(tableName, whereData) {
+    const whereClause = [];
+  
+    for (const [key, value] of Object.entries(whereData)) {
+      whereClause.push(`${key} = '${value}'`);
+    }
+  
+    return whereClause.join(' AND ');
+  }
+}
+
+module.exports = IterateNode;
+
+    const rows = Array.isArray(data[tableName]) ? data[tableName] : [data[tableName]];
+  
+    for (const row of rows) {
+      await this.handleUpsertRow(tableName, row);
+    }
+  }
+
+  async handleUpsertRow(tableName, row) {
+    const primaryKey = this.getPrimaryKey(tableName, row);
+    const existingRow = await this.getRow(tableName, primaryKey);
+  
+    if (!existingRow) {
+      await this.handleInsertRow(tableName, row);
+    } else {
+      const updatedRow = { ...existingRow, ...row };
+      await this.upsertRow(tableName, updatedRow);
+    }
+  }
+
+  async getRow(tableName, primaryKey) {
+    const sql = `SELECT * FROM ${tableName} WHERE ${this.getPrimaryKeyClause(tableName)} = ?`;
+    const result = await this.db.prepare(sql).get(primaryKey);
+    return result;
+  }
+
+  async upsertRow(tableName, row) {
+    const primaryKey = this.getPrimaryKey(tableName, row);
+    const columns = Object.keys(row);
+    const values = Object.values(row);
+  
+    const sql = `REPLACE INTO ${tableName} (${columns.join(',')}) VALUES (${columns.map(() => '?').join(',')})`;
+    await this.db.prepare(sql).run(values);
+  }
+
+  getPrimaryKey(tableName, row) {
+    const primaryKeyColumns = this.getPrimaryKeyColumns(tableName);
+    if (primaryKeyColumns.length === 1) {
+      return row[primaryKeyColumns[0]];
+    }
+  
+    return primaryKeyColumns.map(column => row[column]).join(',');
+  }
+
+  getPrimaryKeyColumns(tableName) {
+    const tableSchema = this.getTableSchema(tableName);
+    const primaryKey = tableSchema.find(column => column.pk);
+    return primaryKey ? primaryKey.name.split(',') : [];
+  }
+
+  getPrimaryKeyClause(tableName) {
+    const primaryKeyColumns = this.getPrimaryKeyColumns(tableName);
+    if (primaryKeyColumns.length === 1) {
+      return primaryKeyColumns[0];
+    }
+  
+    return primaryKeyColumns.map(column => `${column} = ?`).join(' AND ');
+  }
+
+  getTableSchema(tableName) {
+    const sql = `PRAGMA table_info(${tableName})`;
+    const result = this.db.prepare(sql).all();
+    return result;
+  }
+
+  getWhereClause(tableName, whereData) {
+    const whereClause = [];
+  
+    for (const [key, value] of Object.entries(whereData)) {
+      whereClause.push(`${key} = '${value}'`);
+    }
+  
+    return whereClause.join(' AND ');
+  }
+}
+
+module.exports = IterateNode;
+
