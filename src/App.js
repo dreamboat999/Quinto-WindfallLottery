0e2f13c938f0b52b67e72de23c63e0ce3fdac02b
yohdev-dennis <dennis@yohdev.com>
2024-03-17 20:36:20 -0400

    update schema

--- src/App.js
+++ src/App.js
@@ -1,36 +1,36 @@
-import React, { useState, useEffect } from 'react';
-import initSqlJs from 'sql.js';
-import Home from 'pages/Home';
-import { HashLoader } from 'react-spinners';
-import sqlWasm from '!!file-loader?name=sql-wasm-[contenthash].wasm!sql.js/dist/sql-wasm.wasm';
-import 'styles/global.scss';
+import sqlWasm from "!!file-loader?name=sql-wasm-[contenthash].wasm!sql.js/dist/sql-wasm.wasm"
+import Home from "pages/Home"
+import { useEffect, useState } from "react"
+import { HashLoader } from "react-spinners"
+import initSqlJs from "sql.js"
+import "styles/global.scss"
 
 export default function App() {
-  const [db, setDb] = useState(null);
-  const [error, setError] = useState(null);
+  const [db, setDb] = useState(null)
+  const [error, setError] = useState(null)
 
   useEffect(async () => {
     try {
-      const SQL = await initSqlJs({ locateFile: () => sqlWasm });
-      fetch('./LotteryTest.db').then(async (res) => {
-        const db = new SQL.Database(new Uint8Array(await res.arrayBuffer()));
-        setDb(db);
-      });
+      const SQL = await initSqlJs({ locateFile: () => sqlWasm })
+      fetch("./LotteryTest.db").then(async (res) => {
+        const db = new SQL.Database(new Uint8Array(await res.arrayBuffer()))
+        setDb(db)
+      })
     } catch (err) {
-      console.log(err);
-      setError(err);
+      console.log(err)
+      setError(err)
     }
-  }, []);
+  }, [])
 
   return (
-    <div className='App'>
-      {error && '<pre>{error.toString()}</pre>'}
+    <div className="App">
+      {error && "<pre>{error.toString()}</pre>"}
       {!db && (
-        <div className='spinner'>
-          <HashLoader color='#03b3ff' />
+        <div className="spinner">
+          <HashLoader color="#03b3ff" />
         </div>
       )}
       <Home db={db} />
     </div>
-  );
+  )
 }
